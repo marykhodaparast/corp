214e985fd44ddac4d277137018070c2080e09ffa
marykhodaparast <maryam.kh1374@gmail.com>
2024-12-11 16:52:28 +0330

    first commit

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/hero";
+
+export default function PerformancePage() {
+   return (
+     <Hero
+       imgData="/performance.jpg"
+       imgAlt="Welding"
+       title="We serve high performance applications"
+     />
+   );
+}
---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/hero";
+
+export default function Reliability() {
+  return (
+    <Hero
+      imgData="/reliability.jpg"
+      imgAlt="Welding"
+      title="Super high reliability hosting"
+    />
+  );
+}
